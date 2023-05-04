edd7248787ef157b71ab43c14985ad1af29d003b
余志成 <1029686739@qq.com>
2023-05-04 19:04:17 +0800

    pixi

--- src/vite/src/app.tsx
+++ src/vite/src/app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as PIXI from 'pixi.js';
+import * as TWEEN from '@tweenjs/tween.js';
 import './app.less';
 
 import rainDrapUrl from './image/rainDrap.png';
@@ -19,6 +20,7 @@ interface IRainDrap {
   sprite: PIXI.Sprite;
   speed: number;
   name: string;
+  id: number;
 }
 
 interface IText {
@@ -31,8 +33,15 @@ class LuckyRain {
   private app: PIXI.Application<PIXI.ICanvas>;
   private rainDrapList: IRainDrap[] = [];
   private TipTextList: IText[] = [];
-  rainInterval = 300;
+  stageTicker: PIXI.Ticker | undefined;
+  rainInterval = 500;
   tempRainInterval = 0;
+  survivalTime = 15;
+  endTag = true;
+  cb: any;
+  lastTime: number | undefined;
+  startCountdown: PIXI.Text | undefined = undefined;
+  startCountDownTimer: NodeJS.Timer | undefined = undefined;
   constructor(container: HTMLElement, opts?: IOpts) {
     this.app = new PIXI.Application({
       width: container.offsetWidth,
@@ -40,12 +49,36 @@ class LuckyRain {
       background: '#1099bb',
     });
     container.appendChild(this.app.view as unknown as Node);
-    this.init();
+    // this.init();
   }
 
-  private init() {
+  public start(cb: any) {
+    this.cb = cb;
     // 去加载 红包图片
-    this.start();
+    // this.start();
+    const text = new PIXI.Text(`3`, {
+      fontSize: 216,
+      fill: '#FFD442',
+    });
+
+    text.x = this.app.renderer.width / 2 - 54;
+    text.y = this.app.renderer.height / 2 - 108;
+    this.app.stage.addChild(text);
+    this.startCountdown = text;
+    let count = 3;
+    this.startCountDownTimer = setInterval(() => {
+      count -= 1;
+      this.startCountdown!.text = count;
+      if (count === 0 && this.startCountDownTimer) {
+        clearInterval(this.startCountDownTimer);
+        setTimeout(() => {
+          this.app.stage.removeChild(this.startCountdown!);
+          this.startCountdown!.destroy();
+          this.startCountDownTimer = undefined;
+          this._start();
+        }, 500);
+      }
+    }, 1000);
   }
 
   crateRainDrap() {
@@ -64,6 +97,7 @@ class LuckyRain {
       sprite,
       name: names[getRandomInt(0, names.length - 1)],
       speed: getRandom(2, 5),
+      id: PIXI.utils.uid(),
     };
     const app = this.app;
     const self = this;
@@ -100,9 +134,10 @@ class LuckyRain {
     return rainDrap;
   }
   rain(deltaTime: number) {
+    if (this.endTag) return;
     // jian;
     if (this.tempRainInterval === 0) {
-      const count = getRandomInt(1, 5);
+      const count = getRandomInt(1, 4);
       for (let index = 0; index < count; index++) {
         const rainDrap = this.crateRainDrap();
         this.rainDrapList.push(rainDrap);
@@ -114,51 +149,105 @@ class LuckyRain {
       this.tempRainInterval = 0;
     }
   }
+  end() {
+    // if (this.stageTicker) {
+    //   this.TipTextList.forEach((s) => this.app.stage.removeChild(s.text));
+    //   this.rainDrapList.forEach((s) => this.app.stage.removeChild(s.sprite));
+    //   this.rainDrapList = [];
+    //   this.TipTextList = [];
+    //   this.app.ticker.remove(this.tickerCallback);
 
-  start() {
-    let lastTime = performance.now();
-    this.app.ticker.add((delta) => {
-      const now = performance.now();
-      const deltaTime = now - lastTime; // 计算自上一帧以来的时间差
-      lastTime = now;
-
-      // just for fun, let's rotate mr rabbit a little
-      // delta is 1 if running at 100% performance
-      // creates frame-independent transformation
-      this.rain(deltaTime);
-      for (let i = 0; i < this.rainDrapList.length; i++) {
-        const rainDrap = this.rainDrapList[i];
-        const sprite = rainDrap.sprite;
-        if (!sprite._destroyed) {
-          sprite.y += delta * rainDrap.speed;
-          if (sprite.y > this.app.renderer.height) {
-            this.app.stage.removeChild(sprite);
-            sprite.destroy();
-            this.rainDrapList.splice(i, 1);
-          }
-        }
-      }
-
-      for (let i = 0; i < this.TipTextList.length; i++) {
-        const tipText = this.TipTextList[i];
-
-        const { text } = tipText;
-        if (!text._destroyed) {
-          text.y = text.y - 2;
-
-          let alpha = 1 - (tipText.originPoint.y - text.y) / 200;
-          alpha = alpha < 0.5 ? 0.5 : alpha;
-
-          text.alpha = alpha;
-
-          if (text.y < tipText.originPoint.y - 100) {
-            this.app.stage.removeChild(text);
-            text.destroy();
-            this.TipTextList.splice(i, 1);
-          }
-        }
-      }
+    //   this.cb();
+    // }
+    this.endTag = true;
+  }
+  countdownTimer() {
+    let time = this.survivalTime;
+    const text = new PIXI.Text(`${time} s`, {
+      fontSize: 36,
+      fill: '#FFD442',
     });
+
+    text.x = 20;
+    text.y = 20;
+    this.app.stage.addChild(text);
+    const timer = setInterval(() => {
+      time = time - 1;
+      text.text = `${time} s`;
+      if (time < 0) {
+        clearInterval(timer);
+        this.end();
+        text.destroy();
+        this.app.stage.removeChild(text);
+      }
+    }, 1000);
+  }
+
+  tickerCallback = (delta: number) => {
+    if (!this.lastTime) return;
+    const now = performance.now();
+    const deltaTime = now - this.lastTime; // 计算自上一帧以来的时间差
+    this.lastTime = now;
+
+    // just for fun, let's rotate mr rabbit a little
+    // delta is 1 if running at 100% performance
+    // creates frame-independent transformation
+    this.rain(deltaTime);
+    for (let i = 0; i < this.rainDrapList.length; i++) {
+      const rainDrap = this.rainDrapList[i];
+      const sprite = rainDrap.sprite;
+      if (!sprite._destroyed) {
+        sprite.y += delta * rainDrap.speed;
+        console.log(sprite.y);
+
+        if (sprite.y > this.app.renderer.height) {
+          this.app.stage.removeChild(sprite);
+          sprite.destroy();
+        }
+      }
+    }
+
+    if (
+      this.endTag &&
+      this.rainDrapList.every((item) => item.sprite._destroyed)
+    ) {
+      this.cb();
+      this.rainDrapList = [];
+      this.TipTextList.forEach((s) => this.app.stage.removeChild(s.text));
+      this.rainDrapList.forEach((s) => this.app.stage.removeChild(s.sprite));
+      this.rainDrapList = [];
+      this.TipTextList = [];
+      this.app.ticker.remove(this.tickerCallback);
+
+      //   this.cb();
+      alert('游戏结束');
+    }
+
+    for (let i = 0; i < this.TipTextList.length; i++) {
+      const tipText = this.TipTextList[i];
+
+      const { text } = tipText;
+      if (!text._destroyed) {
+        text.y = text.y - 2;
+
+        let alpha = 1 - (tipText.originPoint.y - text.y) / 200;
+        alpha = alpha < 0.5 ? 0.5 : alpha;
+
+        text.alpha = alpha;
+
+        if (text.y < tipText.originPoint.y - 100) {
+          this.app.stage.removeChild(text);
+          text.destroy();
+          this.TipTextList.splice(i, 1);
+        }
+      }
+    }
+  };
+  private _start() {
+    this.endTag = false;
+    this.lastTime = performance.now();
+    this.countdownTimer();
+    this.stageTicker = this.app.ticker.add(this.tickerCallback);
     // console.log(this.app);
   }
   createRaindrop() {}
@@ -166,11 +255,32 @@ class LuckyRain {
 
 export default function App() {
   const app = React.useRef<LuckyRain>();
+  const [showStartBtn, setShowStartBtn] = React.useState(true);
   React.useEffect(() => {
     if (!app.current) {
       const container = document.getElementById('app')!;
-      new LuckyRain(container);
+      app.current = new LuckyRain(container);
     }
   }, []);
-  return <div id='app' />;
+
+  const start = () => {
+    app.current?.start(() => {
+      setShowStartBtn(true);
+    });
+    setShowStartBtn(false);
+  };
+  return (
+    <div id='app'>
+      {showStartBtn && (
+        <button
+          className='start-btn'
+          onClick={() => {
+            start();
+          }}
+        >
+          开始
+        </button>
+      )}
+    </div>
+  );
 }
